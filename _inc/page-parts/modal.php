fc5283349b9a2d86d66ce627438fb29461484b39
Eric Ritchey <eric@eandjdesign.com>
2016-07-31 15:48:26 -0700

    updating thank you text

--- _inc/page-parts/modal.php
+++ _inc/page-parts/modal.php
@@ -3,7 +3,7 @@
   <div class="modal-content">
     <div class="container">
       <div class="modal-content--thanks">
-        <h1>Thanks You for contacting us</h1>
+        <h1>Thank You for contacting us</h1>
         <p>Someone will be getting in touch with you shortly.</p>
         <span class="modal-close btn btn-outline btn-rounded">Close</span>
 
